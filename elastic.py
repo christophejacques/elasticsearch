deab1d7e9e8cecb816970195654d69d84daf4570
christophejacques <cjacques.programmes@numericable.fr>
2023-10-29 01:04:52 +0200

    Fonction permettant de check les index + maj de la class Elastic

--- elastic.py
+++ elastic.py
@@ -1,31 +1,118 @@
-from datetime import datetime
 from elasticsearch import Elasticsearch
 import locale
 
+
+def eprint(*args, **kwargs):
+    print(*args, **kwargs, end="")
+
+
+def check():
+    taille_index: int = 50
+    taille_int: int = 8
+    taille_uuid: int = 24
+    taille_health: int = 8
+    taille_status: int = 7
+
+    es = Elasticsearch([{"scheme": "http", "host": "localhost", "port": 9200}], request_timeout=5.0)
+
+    def print_ligne_horizontale():
+        eprint(f'+{"-"*(2+taille_index)}')
+        eprint(f'+{"-"*(1+taille_int)}')
+        eprint(f'+{"-"*(1+taille_int)}')
+        eprint(f'+{"-"*(1+taille_int)}')
+        eprint(f'+{"-"*(1+taille_uuid)}')
+        eprint(f'+{"-"*(1+taille_health)}')
+        eprint(f'+{"-"*(1+taille_status)}')
+        eprint(f'+{"-"*(1+taille_int)}')
+        eprint(f'+{"-"*(1+taille_int)}+\n')
+
+    def print_data_table(index_name: str):
+        eprint(f"| {index_name[:taille_index]:{taille_index}}")
+
+        eprint(f" |{es.indices.stats(index=index_name)['_shards']['total']:>{taille_int}}")
+        eprint(f" |{es.indices.stats(index=index_name)['_shards']['successful']:>{taille_int}}")
+        eprint(f" |{es.indices.stats(index=index_name)['_shards']['failed']:>{taille_int}}")
+
+        eprint(f" | {es.indices.stats(index=index_name)['indices'][index_name]['uuid']:{taille_uuid}}")
+        eprint(f"| {es.indices.stats(index=index_name)['indices'][index_name]['health']:{taille_health}}")
+        eprint(f"| {es.indices.stats(index=index_name)['indices'][index_name]['status']:{taille_status}}")
+
+        eprint(f"|{es.indices.stats(index=index_name)['indices'][index_name]['total']['docs']['count']:>{taille_int}}")
+        eprint(f" |{es.indices.stats(index=index_name)['indices'][index_name]['total']['docs']['deleted']:>{taille_int}}")
+        print(" |")
+
+    print_ligne_horizontale()
+    eprint(f"| {'index':^{1+taille_index}}")
+    eprint(f"| {'shards':^{1+3*(1+taille_int)}}")
+    eprint(f"| {'indices':^{1+(3+taille_uuid+taille_health+taille_status)}}")
+    print(f"| {'total docs':^{2*(1+taille_int)}}|")
+
+    eprint("|", " "*(1+taille_index))
+    eprint(f"| {'total':^{taille_int}}")
+    eprint(f"| {'success':^{taille_int}}")
+    eprint(f"| {'failed':^{taille_int}}")
+    eprint(f"| {'uuid':^{taille_uuid}}")
+    eprint(f"| {'health':^{taille_health}}")
+    eprint(f"| {'status':^{taille_status}}")
+    eprint(f"| {'count':^{taille_int}}")
+    print(f"| {'deleted':^{taille_int}}|")
+
+    print_ligne_horizontale()
+    for index_name in es.indices.get(index=".*").keys():
+        print_data_table(index_name)
+
+    print_ligne_horizontale()
+    for index_name in es.indices.get(index="*").keys():
+        if not index_name.startswith("."):
+            print_data_table(index_name)
+
+    print_ligne_horizontale()
+    exit()
+
+
+check()
+
+
+def correction():
+    es = Elasticsearch([{"scheme": "http", "host": "localhost", "port": 9200}], request_timeout=5.0)
+    # es.indices.forcemerge(index=".apm-source-map")
+    # es.indices.forcemerge(index=".internal.alerts-stack.alerts-default-000001")
+    print(es.indices.stats(index=".apm-source-map")["indices"])
+    # es.indices.delete(index=".internal.alerts-stack.alerts-default-000001")
+    exit()
+
+
+correction()
 
 locale.setlocale(locale.LC_ALL, '')
 locale._override_localeconv = {'mon_thousands_sep': ' '}
 
 
+def fprint(*args, **kwargs):
+    print(*args, **kwargs, flush=True)
+
+
 class Elastic:
 
     def __init__(self, host: str, port: int):
-        self.es = Elasticsearch([{"scheme": "http", "host": host, "port": port}], request_timeout=10.0)
+        self.es = Elasticsearch([{"scheme": "http", "host": host, "port": port}], request_timeout=5.0)
+        fprint("Connecting to ElasticSearch ... ", end="")
         if not self.es.ping():
-            print("Elastic connection failed.")
+            print("failed.")
             exit()
 
-        print(f"ElasticSearch v{self.es.info().get('version').get('number')} connected")
+        fprint(f"done in v{self.es.info().get('version').get('number')} ")
         self.index: str = ""
 
     def close(self):
         self.es.close()
 
     def use_index(self, index_name: str):
-        if index_name not in self.es.indices.get(index=index_name).keys():
+        if not self.es.indices.exists(index=index_name):
             raise Exception(f"L'index '{index_name}' n'existe pas.")
 
         self.index = index_name
+        fprint(f"Current index set to : {self.index}")
 
     def get_mapping(self) -> list:
         liste: list = list()
@@ -48,12 +135,54 @@ class Elastic:
         print()
         exit()
 
-    def search(self, index: str, query: str, size: int = 10):
-        properties = es.es.search(index=index, q=query, size=size).get("hits")
-        return properties
+    def search(self, index_name: str, query, size: int = -1):
+        if size == -1:
+            size = self.count(index_name)
+        fprint(f"Searching in {index_name}, {query} ... ", end="")
+        if type(query) == str:
+            properties = self.es.search(index=index_name, q=query, size=size)
+        elif type(query) == dict:
+            properties = self.es.search(index=index_name, query=query, size=size)
+        else:
+            raise Exception("Le parametre Query doit etre de type str ou dict.")
 
-    def delete_index(self, index_name: str):
+        taille: int = properties["hits"]["total"].get("value", 0)
+        print("found", taille, "documents", end="")
+        if size < taille:
+            print(f" (but get only {size})", end="")
+        fprint()
+        return properties.get("hits")
+
+    def count(self, index_name=None) -> int:
+        index_name = self.index if index_name is None else index_name
+
+        return self.es.indices.stats(index=index_name)["indices"][index_name]["total"]["docs"].get("count", 0)
+        return self.es.count(index=index_name).get("count", 0)
+
+    def flush_index(self, index_name=None):
+        fprint(f"Flushing index '{self.index}' ... ", end="")
+        index_name = self.index if index_name is None else index_name
+        self.es.indices.flush(index=index_name)
+        fprint("done.")
+
+    def delete_index(self, index_name=None):
+        index_name = self.index if index_name is None else index_name
+        fprint(f"Suppression de tous les documents de l'index: '{index_name}'")
+        nombre: int = self.count(index_name)
+        fprint(f"- Suppression de {nombre} documents", end=" ... ")
         self.es.delete_by_query(index=index_name, q="_id: *")
+        print("done.")
+
+        print("- ", end="")
+        self.update_index(index_name)
+
+        print("- ", end="")
+        self.flush_index(index_name)
+
+    def drop_index(self, index_name: str):
+        fprint(f"Suppression de l'index: '{index_name}'", end=" ... ")
+        self.es.indices.delete(index=index_name)
+        fprint("done")
 
     def aggregations(self):
         # query = {"match": {"age": 36}}
@@ -100,20 +229,44 @@ class Elastic:
 
     def add_doc(self, document):
         resp = self.es.index(index=self.index, document=document)
-        print(resp['result'], flush=True)
+        fprint(resp['result'])
+
+    def update_index(self, index_name=None):
+        # Forcez Elasticsearch à mettre à jour les index
+        fprint(f"Updating index '{self.index}' ... ", end="")
+        index_name = self.index if index_name is None else index_name
+        self.es.indices.refresh(index=index_name)
+        fprint("done.")
 
 
 if __name__ == "__main__":
     es = Elastic("localhost", 9200)
-    es.use_index("suivi-activite")
-    mapping: list = es.get_mapping()
-    
-    es.delete_index("creation")
-    es.use_index("creation")
+    if False:
+        # from pprint import pprint
+        # print(dir(es.es))
+        # print(dir(es.es.indices))
+        # help(es.es.indices.delete)
+        # pprint(es.es.indices.stats(index="creation")["indices"]["creation"]["total"])
 
-    properties = es.search(index="suivi-activite", query="_id:*", size=10)
-    nb_docs = properties["total"].get("value")
-    print("Nb docs:", nb_docs)
+        exit()
+
+    es.use_index("suivi-activite")
+    taille: int = es.count()
+    mapping: list = es.get_mapping()
+    fprint("Nb docs dans l'index :", taille)
+
+    index_name = "creation"    
+    es.use_index(index_name)
+    es.delete_index()
+
+    query = {
+        "match": {
+            "date": "2023-10-05"
+        }
+    }
+    # query = "code_partenaire = 'ARA'"
+    # properties = es.search("suivi-activite", query="_id:*", size=5)
+    properties = es.search("suivi-activite", query=query, size=5)
 
     num: int = 0
     for docs in properties.get("hits"):
@@ -122,6 +275,11 @@ if __name__ == "__main__":
         print(f"{num:3}", docs.get("_id"), ": ", end="")
         for cle in mapping:
             document[cle] = docs["_source"].get(cle)
-            print(document[cle], end=" / ")
+            print(str(document[cle])[:40], end=" / ")
 
         es.add_doc(document)
+
+    es.update_index()
+    es.flush_index()
+    print("Nombre de documents ajoutes :", es.count())
+    es.close()
